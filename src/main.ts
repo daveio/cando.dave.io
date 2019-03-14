d76a634cec9d4cdddb680ba86d2b21edbf90e681
Dave Williams <dave@dave.io>
2019-03-14 00:48:55 +0000

    linter fixup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import '@babel/polyfill'
+import "@babel/polyfill";
 import Vue from "vue";
-import './plugins/bootstrap-vue'
+import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-import './plugins/element.js'
+import "./plugins/element.js";
 
 Vue.config.productionTip = false;
 
