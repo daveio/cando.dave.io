e9cd5b1342f2209cecadb0eb5892ec3af2da8fef
Dave Williams <dave@dave.io>
2019-03-14 00:00:59 +0000

    greenfield with vue cli generator

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  runtimeCompiler: true,
+  publicPath: undefined,
+  outputDir: undefined,
+  assetsDir: "assets",
+  productionSourceMap: undefined,
+  parallel: undefined,
+
+  css: {
+    sourceMap: true
+  },
+
+  lintOnSave: undefined,
+
+  pwa: {
+    name: "CanDo"
+  }
+};
