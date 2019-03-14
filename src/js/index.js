fa37187f0d5a3802b15dee0d53f6d6c9192beffe
Dave Williams <dave@dave.io>
2019-03-14 21:16:17 +0000

    figure out how to call exports from html

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,16 @@
 /** @format */
 
+require('es6-promise').polyfill()
+
+import 'bootstrap'
 import _ from 'lodash'
-import printMe from './print.js'
-import '../css/style.css'
+import '../css/app.scss'
 import Icon from '../images/icon.jpg'
-import Data from '../data/sample-note.xml'
 import '../index.html'
 import '../CNAME'
+import axios from 'axios'
+import qs from 'qs'
 
-function component() {
-    let element = document.createElement('div')
-    var btn = document.createElement('button')
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    element.classList.add('hello')
-
-    btn.innerHTML = 'Click me and check the console!'
-    btn.onclick = printMe
-
-    // Add the image to our existing div.
-    var myIcon = new Image()
-    myIcon.src = Icon
-
-    element.appendChild(myIcon)
-    element.appendChild(btn)
-
-    console.log(Data)
-
-    return element
+export function sendRequest(data) {
+    axios.post('https://postb.in/GnfdAHfl', qs.stringify(data))
 }
-
-document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'production',
-    devtool: 'source-map',
+    mode: 'development',
+    // devtool: 'source-map',
     devServer: {
         contentBase: './dist',
     },
@@ -11,13 +12,18 @@ module.exports = {
         new CleanWebpackPlugin({
             cleanOnceBeforeBuildPatterns: ['**/*', '!.git/**', '!.gitignore'],
         }),
+        new MiniCssExtractPlugin({
+            filename: "[name].bundle.css",
+        })
     ],
     entry: {
         app: './src/js/index.js'
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'umd',
+        library: 'cando'
     },
     module: {
         rules: [{
@@ -35,10 +41,44 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-transform-runtime']
+                        presets: [
+                            '@babel/preset-env'
+                        ],
+                        plugins: [
+                            '@babel/plugin-transform-runtime'
+                        ]
                     }
                 }
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it use publicPath in webpackOptions.output
+                            // publicPath: '../'
+                        }
+                    },
+                    /*{
+                        loader: 'style-loader', // inject CSS to page
+                    },*/
+                    {
+                        loader: 'css-loader', // translates CSS into CommonJS modules
+                    }, {
+                        loader: 'postcss-loader', // Run post css actions
+                        options: {
+                            plugins: function() { // post css plugins, can be exported to postcss.config.js
+                                return [
+                                    require('precss'),
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    }, {
+                        loader: 'sass-loader' // compiles Sass to CSS
+                    }
+                ]
             },
             {
                 test: /\.css$/,
