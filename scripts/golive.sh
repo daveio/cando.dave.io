1b5ad2df35eaf1587ff37e8906f8a30be8792e57
Dave Williams <dave@dave.io>
2019-03-14 18:25:52 +0000

    update golive script with fixed date format string

--- scripts/golive.sh
+++ scripts/golive.sh
@@ -1,3 +1,3 @@
-BUILDDATE=$(date +%Y%M%d.%H%m)
+BUILDDATE=$(date +%Y%m%d.%H%M)
 BUILDTAG=$(dd if=/dev/urandom bs=1024 count=1 2> /dev/null | shasum -a 256 | cut -b 1-6)
 yarn && yarn run build && git add -A . && git commit -am "snapshot ${BUILDDATE}.${BUILDTAG}" && git push && cd ../cando.dave.io.pages && git add -A . && git commit -am "deploy ${BUILDDATE}.${BUILDTAG}" && git push
